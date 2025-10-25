1c9866839bf68bb940754ce2cd722de1cbfa3be0
cherry4xo <1jlaketuk1@gmail.com>
2025-10-25 17:58:50 +0300

    paths fixes

--- services/backend/main.py
+++ services/backend/main.py
@@ -5,14 +5,10 @@ from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
-from app.fill import run_seeding
 from app.db import init
 from app import settings
-from app.routes.auditorium import router as auditorium_router
-from app.routes.availability import router as availability_router
-from app.routes.booking import router as booking_router
-from app.routes.equipment import router as equipment_router
 from app.routes.users import router as users_router
+from app.routes.orbit import router as orbit_router
 from app.logger import setup_logging, LoggingMiddleware
 
 
@@ -37,18 +33,13 @@ main_app_lifespan = app.router.lifespan_context
 async def lifespan_wrapper(app):
     await init(app)
     instrumentator.expose(app)
-    if settings.MODE == "DEBUG":
-        await run_seeding()
     async with main_app_lifespan(app) as maybe_state:
         yield maybe_state
 app.router.lifespan_context = lifespan_wrapper
 
 init_middlewares(app)
-app.include_router(auditorium_router, prefix="/auditoriums", tags=["auditorium"])
-app.include_router(availability_router, prefix="/availability", tags=["availability"])
-app.include_router(booking_router, prefix="/bookings", tags=["booking"])
-app.include_router(equipment_router, prefix="/equipment", tags=["equipment"])
 app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(orbit_router, prefix="/orbit", tags=["orbit"])
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host=settings.API_HOST, port=int(settings.API_PORT))
--- services/calculation/main.py
+++ services/calculation/main.py
@@ -1,5 +1,5 @@
 import asyncio
-from worker import run_worker
+from app.worker import run_worker
 
 if __name__ == "__main__":
     asyncio.run(run_worker())
