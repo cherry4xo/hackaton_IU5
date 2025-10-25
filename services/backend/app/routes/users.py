1b22314c696aa094d26b3e6d934faf21e41f8828
cherry4xo <1jlaketuk1@gmail.com>
2025-10-25 18:14:42 +0300

    fixes

--- services/backend/app/routes/users.py
+++ services/backend/app/routes/users.py
@@ -4,7 +4,7 @@ from typing import List
 
 from app.schemas import UserCreate, UserCreated, UserGet, UserChangePasswordIn, UserGrantPrivileges
 from app.models import User
-from app.services.users import create_user, change_password, grant_user, update_profile
+from app.services.users import create_user, change_password, grant_user
 from app.utils.contrib import get_current_user, get_current_admin
 
 
