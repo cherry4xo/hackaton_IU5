4bcdf7b5b76f001b4f7fca1816f787006a86af70
cherry4xo <1jlaketuk1@gmail.com>
2025-10-26 14:25:28 +0300

    fixes

--- services/backend/app/routes/orbit.py
+++ services/backend/app/routes/orbit.py
@@ -138,7 +138,7 @@ async def get_task_status(
         )
     
     # Check if user has access to this task
-    if task.user != user:
+    if task.user_id != user.uuid:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Not authorized to access this task"
