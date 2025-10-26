c3c35a02111dc4eebe35411fc27c306a4994bdb3
1maryyam2 <mokhammed2007@gmail.com>
2025-10-26 17:49:01 +0300

    fix

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@
 import HomePage from '../pages/HomePage';
 import CalculatePage from '../pages/CalculatePage';
 import HistoryPage from '../pages/HistoryPage';
-import ResultsPage from '@/pages/ResultPage';
+import ResultsPage from '../pages/ResultPage';
 import { Routes, Route } from 'react-router-dom';
 import { MainLayout } from '../widgets/MainLayout/ui/MainLayout';
 import React from 'react';
