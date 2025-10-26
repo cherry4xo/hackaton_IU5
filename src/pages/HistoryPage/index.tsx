1c7788d17100e0499a5bf381b2dc48e49d2ff14b
pekinesssa <sskaverinass@gmail.com>
2025-10-26 15:05:04 +0300

    Histrory pages

--- src/pages/HistoryPage/index.tsx
+++ src/pages/HistoryPage/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import HistoryTimelineWidget  from "../../widgets/HistoryPage/ui/HistoryPage"
 // import { HistoryPage } from '../../widgets/HistoryPage/ui/HistoryPage';
+import styles from './HistortyPage.module.css';
 
 const HistoryPage: React.FC = () => {
   return (
-      <HistoryTimelineWidget  />
+    <div className={styles.pageWrapper}>
+        <HistoryTimelineWidget  />
+    </div>
   );
 };
 
